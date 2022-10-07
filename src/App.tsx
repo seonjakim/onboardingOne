ffa5d976c530947edb8bbeb84b85a0606aaa5937
김선자 <seonja.kim0@gmail.com>
2022-10-07 17:23:31 +0900

    feat: initial setting

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Wanted Onboarding First Assignment</h1>
+      <div className="card">
+        <h2>I am root!</h2>
+        <button>
+          link to about!
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/about.tsx
@@ -0,0 +1,17 @@
+import './App.css';
+
+function About() {
+  return (
+    <div className="App">
+      <h1>Wanted Onboarding First Assignment</h1>
+      <div className="card">
+        <h2>I am about!</h2>
+        <button>
+          link to root!
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default About
